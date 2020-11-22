9a336b27e25179b87d13159f0a78144e7b7f44a7
serhiidernovyi <gifulmen@gmail.com>
2020-11-22 20:30:36 +0100

    7

--- src/main/resources/db.migration/V1__init.sql
+++ src/main/resources/db.migration/V1__init.sql
@@ -1,3 +1,9 @@
 -- DROP TABLE IF EXISTS IMAGES;
-CREATE TABLE IMAGES(id BIGINT(20) primary key auto_increment, language VARCHAR(50), text VARCHAR(5000), url VARCHAR(255) not null unique, created_On datetime, updated_On datetime);
+CREATE TABLE IF NOT EXISTS images(
+    id BIGINT(20) primary key auto_increment,
+    language VARCHAR(50),
+    text VARCHAR(5000),
+    url VARCHAR(255) not null unique,
+    created_On datetime,
+    updated_On datetime);
 
