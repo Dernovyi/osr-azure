66f63925e2607dc2f568f9932c17963e984e6990
serhiidernovyi <gifulmen@gmail.com>
2020-11-22 21:25:04 +0100

    addflyway

--- src/main/resources/db.migration/V1__init.sql
+++ src/main/resources/db.migration/V1__init.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE  IMAGES (
-    id INT  PRIMARY KEY,
+    id INT  PRIMARY KEY SERIAL ,
     language VARCHAR(50),
     text VARCHAR(5000),
     url VARCHAR(255) not null unique,
-    created_On datetime,
-    updated_On datetime);
+    created_On timestamp,
+    updated_On timestamp);
 
