96cc0f2d6d620cfcc09b3f8d2545ff957407baad
serhiidernovyi <gifulmen@gmail.com>
2020-11-27 15:45:33 +0100

    add new option(chang)

--- src/main/java/pl/dernovyi/osrazure/controller/AzureBlobController.java
+++ src/main/java/pl/dernovyi/osrazure/controller/AzureBlobController.java
@@ -1,8 +1,5 @@
 package pl.dernovyi.osrazure.controller;
 
-
-import com.microsoft.azure.storage.StorageException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +12,6 @@ import pl.dernovyi.osrazure.service.StoreageService;
 
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 
 @RestController
@@ -34,7 +30,7 @@ public class AzureBlobController {
     }
 
     @PostMapping
-    public ResponseEntity<ImageDto> get(@RequestParam("file") MultipartFile multipartFile) throws URISyntaxException, StorageException {
+    public ResponseEntity<ImageDto> get(@RequestParam("file") MultipartFile multipartFile){
 
         URI uri = service.saveToStorage(multipartFile);
         RequestPicture picture = new RequestPicture();
@@ -50,53 +46,5 @@ public class AzureBlobController {
         return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
-    //    @RestController
-//    public class FileDownload {
-//        @GetMapping(path="/download-file")
-//        public ResponseEntity<Resource> getFile(){
-//            String fileName = "Can not find symbol.docx";
-//            //azure credentials
-//            String connection_string = "Connection String of storage account on azure";
-//
-//            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connection_string).buildClient();
-//
-//            BlobContainerClient containerClient= blobServiceClient.getBlobContainerClient("Container name");
-//            System.out.println(containerClient.getBlobContainerName());
-//
-//            BlobClient blob = containerClient.getBlobClient(fileName);
-//
-//            //creating an object of output stream to recieve the file's content from azure blob.
-//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//            blob.download(outputStream);
-//
-//            //converting it to the inputStream to return
-//            final byte[] bytes = outputStream.toByteArray();
-//            ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-//            ByteArrayResource resource = new ByteArrayResource(bytes);
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"");
-//
-//            return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)
-//                    .headers(headers)
-//                    .body(resource);
-//        }
-//
-//    }
-
-//    @PostMapping
-//    public ResponseEntity uploadFile(@RequestParam MultipartFile multipartFile) throws URISyntaxException, InvalidKeyException, StorageException, IOException {
-//        FileInfo fileInfo = new FileInfo();
-//        if (!multipartFile.isEmpty()) {
-//
-//            CloudStorageAccount storageAccount = CloudStorageAccount.parse(myInfo.getDefaultEndpointsProtocol());
-//            CloudBlobClient client = storageAccount.createCloudBlobClient();
-//            CloudBlobContainer image = client.getContainerReference("image");
-//            CloudBlockBlob blockBlobReference = image.getBlockBlobReference(Objects.requireNonNull(multipartFile.getOriginalFilename()));
-//            blockBlobReference.upload(multipartFile.getInputStream(), -1);
-//            URI uri = blockBlobReference.getUri();
-//            return ResponseEntity.ok(uri);
-//        }
-//        return new ResponseEntity(HttpStatus.NO_CONTENT);
-//    }
 
 }
